819010ead403b0930d61b9fb3a70077eee289304
Raoni <raoni.itar@gmail.com>
2022-07-25 19:24:28 -0300

    ...

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+import { env } from './.eslintrc';
 import app from './app';
 
-const HOST = 'localhost';
+const HOST = 'localhost' | process.env.HOST;
 
-const PORT = '3030';
+const PORT = '3030' | process.env.HOST;
 
 app.listen(PORT, HOST, () => {
   console.log(`http://${HOST}:${PORT}`);
